eb9979d1129a7215cd78a9264bf3fb9c9ffeafda
manjeet0230 <134271295+manjeet0230@users.noreply.github.com>
2023-11-02 22:48:12 +0530

    Update settings.py

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-5w$z0tmbsg^asmf4w1tzfead6&lte9q$9p+1dkijcc()#xmsz3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# ALLOWED_HOSTS = [goldyrana.pythonanywhere.com]
+ALLOWED_HOSTS = [goldyrana.pythonanywhere.com]
 
 
 # Application definition
